59b6cdb3ee7e3680de92e5ea54e59391b8e19757
Walisson Gonzales <78445689+walisson-gonz@users.noreply.github.com>
2021-06-11 16:52:26 -0300

    Update BlogPessoalApplication.java

--- src/main/java/br/org/generation/blogpessoal/BlogPessoalApplication.java
+++ src/main/java/br/org/generation/blogpessoal/BlogPessoalApplication.java
@@ -14,7 +14,7 @@ public class BlogPessoalApplication {
 
 	@GetMapping
 	public ModelAndView swaggerUi() {
-		return new ModelAndView("redirect:/swagger-ui ");
+		return new ModelAndView("redirect:/swagger-ui/");
 	}
 
 	public static void main(String[] args) {
